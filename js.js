7d0f6e4394a82529b59d418ae9db112edb005100
xushengfeng <xushengfeng_zg@163.com>
2021-10-05 15:58:42 +0800

    :lipstick:canvas展示图片

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 // In the renderer process.
-const { desktopCapturer} = require("electron");
+const { desktopCapturer } = require("electron");
+
+const canvas = document.getElementById("main_photo");
 
 desktopCapturer
     .getSources({
@@ -7,22 +9,19 @@ desktopCapturer
         fetchWindowIcons: true,
         thumbnailSize: {
             width: window.screen.width * window.devicePixelRatio,
-            height: window.screen.height * window.devicePixelRatio,
+            height: 8000,
         },
     })
     .then(async (sources) => {
         console.log(sources);
         draw_windows_bar(sources);
-        document.getElementById("main_photo").src =
-            sources[0].thumbnail.toDataURL();
+        draw_canvas(sources[0].thumbnail.toDataURL());
     });
-
-document.getElementById('main_photo').width=window.screen.width
 
 function draw_windows_bar(o) {
     内容 = "";
     for (i in o) {
-        内容 += `<div class="window" onclick="load_photo(${i})" id="${
+        内容 += `<div class="window" id="${
             o[i].id
         }"><div class="window_name"><p class="window_title"><img src="${
             o[i].appIcon?.toDataURL() ?? "assets/no_photo.png"
@@ -36,9 +35,19 @@ function draw_windows_bar(o) {
     for (i in o) {
         (function (n) {
             document.getElementById(o[n].id).addEventListener("click", () => {
-                document.getElementById("main_photo").src =
-                    o[n].thumbnail.toDataURL();
+                draw_canvas(o[n].thumbnail.toDataURL())
             });
         })(i);
     }
+}
+
+function draw_canvas(url) {
+    let ctx = canvas.getContext("2d");
+    let img = new Image();
+    img.src = url;
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+    };
 }
