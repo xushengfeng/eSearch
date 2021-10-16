7d8ddd2941daf49320b98fd391506247939b2461
xushengfeng <xushengfeng_zg@163.com>
2021-10-16 23:01:50 +0800

    :sparkles:可搜索

--- js/js.js
+++ js/js.js
@@ -1,20 +1,20 @@
-// const { ipcRenderer, shell } = require("electron");
+const { ipcRenderer, shell } = require("electron");
 
-// t = "";
-// type = "";
-// ipcRenderer.on("text", (event, list) => {
-//     t = list[0];
-//     type = list[1];
-//     if (type == "ocr") {
-//         show_ocr_r(t);
-//     }
-//     if (type == "QR") {
-//         show_t(t);
-//     }
-//     if (type == "text") {
-//         show_t(t);
-//     }
-// });
+t = "";
+type = "";
+ipcRenderer.on("text", (event, list) => {
+    t = list[0];
+    type = list[1];
+    if (type == "ocr") {
+        show_ocr_r(t);
+    }
+    if (type == "QR") {
+        show_t(t);
+    }
+    if (type == "text") {
+        show_t(t);
+    }
+});
 
 function show_ocr_r(t) {
     var t = JSON.parse(t);
@@ -36,16 +36,6 @@ function replace_link(t) {
     return t.replace(regex, '<a href="https://$1">$1</a>');
 }
 function is_link(l) {}
-document.getElementById("search_b").addEventListener("click", () => {
-    s = // 要么全部，要么选中
-        document.getSelection().toString() == ""
-            ? document.getElementById("text").innerText
-            : document.getSelection().toString();
-    url = `https://www.baidu.com/s?wd=${s}`;
-    console.log(url);
-    shell.openExternal(url);
-    window.open(url, "_blank");
-});
 
 搜索引擎_list = {
     谷歌: "https://www.google.com/search?q=%s",
@@ -71,3 +61,31 @@ for (i in 翻译引擎_list) {
     translate_c += `<option value="${翻译引擎_list[i]}">${i}</option>`;
 }
 document.querySelector("#translate_s").innerHTML = translate_c;
+
+浏览器打开 = true;
+
+function open_link(id) {
+    s = // 要么全部，要么选中
+        document.getSelection().toString() == ""
+            ? document.getElementById("text").innerText
+            : document.getSelection().toString();
+    url = document.querySelector(`#${id}_s`).value.replace("%s", s);
+    if (浏览器打开) {
+        shell.openExternal(url);
+    } else {
+        window.open(url, "_blank");
+    }
+}
+
+document.querySelector("#search_b").onclick = () => {
+    open_link("search");
+};
+document.querySelector("#translate_b").onclick = () => {
+    open_link("translate");
+};
+document.querySelector("#search_s").oninput = () => {
+    open_link("search");
+};
+document.querySelector("#translate_b").oninput = () => {
+    open_link("translate");
+};
