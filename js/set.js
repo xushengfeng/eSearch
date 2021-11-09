590e7188e60e0653419d3253fd2b7950ac92e1b1
xushengfeng <xushengfeng_zg@163.com>
2021-11-09 23:14:21 +0800

    :bug:修复设置不显示的bug

--- js/set.js
+++ js/set.js
@@ -6,20 +6,20 @@ function 选择器储存(id, 默认) {
     };
 }
 
-// document.querySelector("#自动识别 hot-keys input").value = store.get("key_自动识别") || "";
-// document.querySelector("#截图搜索 hot-keys input").value = store.get("key_截图搜索") || "";
-// document.querySelector("#选中搜索 hot-keys input").value = store.get("key_选中搜索") || "";
-// document.querySelector("#剪贴板搜索 hot-keys input").value = store.get("key_剪贴板搜索") || "";
+document.querySelector("#自动识别 hot-keys div").innerHTML = `<kbd>${store.get("key_自动识别")?.replace(/\+/g,'</kbd>+<kbd>')}</kbd>`|| "";
+document.querySelector("#截图搜索 hot-keys div").innerHTML = `<kbd>${store.get("key_截图搜索")?.replace(/\+/g,'</kbd>+<kbd>')}</kbd>`|| "";
+document.querySelector("#选中搜索 hot-keys div").innerHTML = `<kbd>${store.get("key_选中搜索")?.replace(/\+/g,'</kbd>+<kbd>')}</kbd>`|| "";
+document.querySelector("#剪贴板搜索 hot-keys div").innerHTML = `<kbd>${store.get("key_剪贴板搜索")?.replace(/\+/g,'</kbd>+<kbd>')}</kbd>` || "";
 
-// if (document.title == "eSearch-设置") {
-//     选择器储存("工具栏跟随", "展示内容优先");
-//     选择器储存("光标", "以(1,1)为起点");
-//     选择器储存("取色器默认格式", "HEX");
-// }
+if (document.title == "eSearch-设置") {
+    选择器储存("工具栏跟随", "展示内容优先");
+    选择器储存("光标", "以(1,1)为起点");
+    选择器储存("取色器默认格式", "HEX");
+}
 
-// // 取色器设置
-// document.querySelector("#取色器大小").value = store.get("取色器大小") || "15";
-// document.querySelector("#像素大小").value = store.get("像素大小") || "10";
+// 取色器设置
+document.querySelector("#取色器大小").value = store.get("取色器大小") || "15";
+document.querySelector("#像素大小").value = store.get("像素大小") || "10";
 document.querySelector("#取色器大小").oninput = () => {
     if ((document.querySelector("#取色器大小").value - 0) % 2 == 0) {
         document.querySelector("#取色器大小").value = document.querySelector("#取色器大小").value - 0 + 1;
@@ -39,6 +39,7 @@ img.onload = () => {
     point_color_view.width = img.width;
     point_color_view.height = img.height;
     point_color_view.getContext("2d").drawImage(img, 0, 0);
+    show_color_picker()
 };
 function show_color_picker() {
     copy_size = document.querySelector("#取色器大小").value - 0;
