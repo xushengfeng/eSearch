0c8c522bc053d6881d8eb79c4bbd6c2a77c64a3c
xushengfeng <xushengfeng_zg@163.com>
2021-11-10 21:54:55 +0800

    :sparkles:显示缩放百分比

--- js/ding.js
+++ js/ding.js
@@ -10,13 +10,19 @@ ipcRenderer.on("window_size", (event, size) => {
 });
 窗口透明度 = document.getElementById("透明度");
 窗口透明度.oninput = () => {
-    document.getElementById("ding_photo").style.opacity = `${窗口透明度.value/100}`;
+    document.getElementById("ding_photo").style.opacity = `${窗口透明度.value / 100}`;
 };
 document.querySelector("#size").oninput = () => {
     document.querySelector("#size_p").innerHTML = `${document.querySelector("#size").value}%`;
     zoom = (document.querySelector("#size").value - 0) / 100;
-    console.log([window_size[0] * zoom, window_size[1] * zoom])
-    ipcRenderer.send("ding_resize", window_name, [Math.round(window_size[0] * zoom), Math.round(window_size[1] * zoom)]);
+    ipcRenderer.send("ding_resize", window_name, [
+        Math.round(window_size[0] * zoom),
+        Math.round(window_size[1] * zoom),
+    ]);
+};
+window.onresize = () => {
+    document.querySelector("#size").value = (window.innerWidth / window_size[0]) * 100;
+    document.querySelector("#size_p").innerHTML = `${((window.innerWidth / window_size[0]) * 100).toFixed(0)}%`;
 };
 document.querySelector("#close").onclick = () => {
     ipcRenderer.send("ding_close", window_name);
