98d7b8bf99254b1eb048298c2c04a007d629ab86
xushengfeng <xushengfeng_zg@163.com>
2021-12-04 20:06:51 +0800

    :bug:修复无法展开历史记录

--- js/js.js
+++ js/js.js
@@ -176,10 +176,13 @@ function show_history() {
         history_showed = true;
 
         var history_text = "";
-        for (var i in history_list) {
-            history_text = `<div><div>${html2Escape(history_list[i].text)}</div><button></button></div>` + history_text;
+        if (history_list.length != 0) {
+            for (var i in history_list) {
+                history_text =
+                    `<div><div>${html2Escape(history_list[i].text)}</div><button></button></div>` + history_text;
+            }
+            document.querySelector("#history_list").innerHTML = history_text;
         }
-        if (history_list.length != 0) document.querySelector("#history_list").innerHTML = history_text;
         document.querySelectorAll("#history_list > div > div").forEach((e, index) => {
             e.addEventListener("click", () => {
                 document.getElementById("text").innerText = history_list[index].text;
