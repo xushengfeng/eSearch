0b9df7291d31552837a83fc2955445d3fc5ca6dd
xushengfeng <xushengfeng_zg@163.com>
2021-12-05 14:08:56 +0800

    :bento:icon添加win与mac

--- assets/icon.sh
+++ assets/icon.sh
@@ -5,3 +5,5 @@ echo Making bitmaps from your svg...
 for i in ${size[@]}; do
   inkscape --export-type="png" --export-filename="icons/${i}x$i.png" -w $i -h $i icon.svg
 done 
+convert -density 256x256 -background transparent icon.svg -define icon:auto-resize=256,48,32,16 -colors 256 icons/icon.ico
+convert icons/1024x1024.png icons/icon.icns
