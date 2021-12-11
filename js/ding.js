1a04d39d0dc6ef07716c9a250c562e7f373c5870
xushengfeng <xushengfeng_zg@163.com>
2021-12-12 00:10:21 +0800

    :sparkles:大小滑条改文字输入

--- js/ding.js
+++ js/ding.js
@@ -16,25 +16,29 @@ ipcRenderer.on("window_position", (event, position) => {
 窗口透明度.oninput = () => {
     document.getElementById("ding_photo").style.opacity = `${窗口透明度.value / 100}`;
 };
-document.querySelector("#size").oninput = () => {
-    document.querySelector("#size_p").innerHTML = `${document.querySelector("#size").value}%`;
-    var zoom = (document.querySelector("#size").value - 0) / 100;
+document.querySelector("#size > span").onblur = () => {
+    var zoom = (document.querySelector("#size > span").innerHTML - 0) / 100;
     resize(zoom, 0, 0);
+};
+document.querySelector("#size > span").onkeydown = (e) => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        var zoom = (document.querySelector("#size > span").innerHTML - 0) / 100;
+        resize(zoom, 0, 0);
+    }
 };
 function resize(zoom, dx, dy) {
     if (zoom < 0.25) zoom = 0.25;
     if (zoom > 3) zoom = 3;
     ipcRenderer.send("ding_resize", window_name, dx, dy, window_size[0], window_size[1], zoom);
 }
-document.querySelector("#size").value = 100;
-document.querySelector("#size_p").innerHTML = document.querySelector("#size_main_p").innerHTML = "100%";
+document.querySelector("#size > span").innerHTML = document.querySelector("#size_main_p > span").innerHTML = "100";
 window.onresize = () => {
     document.querySelector("#size_main_p").style.display = "block";
     document.querySelector("#size_main_p").style.opacity = "1";
-    document.querySelector("#size").value = (window.innerWidth / window_size[0]) * 100;
-    document.querySelector("#size_p").innerHTML = document.querySelector("#size_main_p").innerHTML = `${
+    document.querySelector("#size > span").innerHTML = document.querySelector("#size_main_p > span").innerHTML = `${
         ((window.innerWidth / window_size[0]) * 100).toFixed(0) || 100
-    }%`;
+    }`;
     show_size_timer = setTimeout(() => {
         document.querySelector("#size_main_p").style.opacity = "0";
     }, 500);
@@ -73,11 +77,7 @@ document.querySelector("#ding_photo").onmouseup = () => {
 
 document.querySelector("#ding_photo").onwheel = (e) => {
     resize(
-        (document.querySelector("#size").value -
-            0 -
-            (e.deltaY / Math.abs(e.deltaY)) * document.querySelector("#size").step -
-            0) /
-            100,
+        (document.querySelector("#size > span").innerHTML - 0 - (e.deltaY / Math.abs(e.deltaY)) * 10) / 100,
         e.clientX,
         e.clientY
     );
