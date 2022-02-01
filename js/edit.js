f8241015b0f3f538b297ae4abce4e5ef7e11268f
xushengfeng <xushengfeng_zg@163.com>
2022-02-01 23:14:43 +0800

    :recycle:几何功能简写

--- js/edit.js
+++ js/edit.js
@@ -84,26 +84,7 @@ document.querySelector("#draw_free_spray").oninput = () => {
 // 几何
 var shape = "";
 document.getElementById("draw_shapes_i").onclick = (e) => {
-    switch (e.target.id) {
-        case "draw_shapes_line":
-            shape = "line";
-            break;
-        case "draw_shapes_circle":
-            shape = "circle";
-            break;
-        case "draw_shapes_rect":
-            shape = "rect";
-            break;
-        case "draw_shapes_polyline":
-            shape = "polyline";
-            break;
-        case "draw_shapes_polygon":
-            shape = "polygon";
-            break;
-        case "draw_shapes_text":
-            shape = "text";
-            break;
-    }
+    if (e.target.id != "draw_shapes_i") shape = e.target.id.replace("draw_shapes_", ""); // 根据元素id命名为shape赋值
     exit_free();
     exit_filter();
     fabric_canvas.defaultCursor = "crosshair";
