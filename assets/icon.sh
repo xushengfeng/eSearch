0558dbe378ec632115bbb444f05ede9da47440c4
xsf_mac <xushengfeng_zg@163.com>
2022-02-26 17:42:59 +0800

    :bento:mac下的icns文件

--- assets/icon.sh
+++ assets/icon.sh
@@ -6,4 +6,3 @@ for i in ${size[@]}; do
   inkscape --export-type="png" --export-filename="icons/${i}x$i.png" -w $i -h $i icon.svg
 done 
 convert -density 256x256 -background transparent icon.svg -define icon:auto-resize=256,48,32,16 -colors 256 icons/icon.ico
-convert icons/1024x1024.png icons/icon.icns
