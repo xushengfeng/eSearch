0f566ea620ff0fac9f3078ba857b4cefc20fe4c4
xushengfeng <xushengfeng_zg@163.com>
2022-04-05 13:28:03 +0800

    :recycle:封装https post get函数

--- ocr/ocr.js
+++ ocr/ocr.js
@@ -77,103 +77,88 @@ function local_ocr(arg, callback) {
 }
 
 /**
+ * @param {string} url
+ * @param {https.RequestOptions} options
+ * @param {Function} cb 回调
+ * @param {object} write req.write(write)
+ */
+function 获取(url, options, cb, write) {
+    const https = require("https");
+    const { URLSearchParams } = require("url");
+
+    var req = https.request(url, options, function (res) {
+        var chunks = [];
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+        res.on("end", function () {
+            var body = Buffer.concat(chunks);
+            return cb(null, JSON.parse(body.toString()));
+        });
+        res.on("error", (err) => {
+            return cb(new Error(JSON.stringify(err)), null);
+        });
+    });
+    req.on("error", () => {
+        return cb(new Error("网络或服务错误"), null);
+    });
+    if (write) req.write(new URLSearchParams(write).toString());
+    req.end();
+}
+
+/**
  * 在线OCR
  * @param {String} type 服务提供者
  * @param {String} arg 图片base64
  * @param {Function} callback 回调
  */
 function online_ocr(type, arg, callback) {
-    const https = require("https");
-    const { URLSearchParams } = require("url");
-
     var client_id = store.get(`在线OCR.${type}.id`),
         client_secret = store.get(`在线OCR.${type}.secret`);
-
     if (!client_id || !client_secret) return callback("未填写 API Key 或 Secret Key", null);
 
     access();
     function access() {
-        var options = {
-            method: "GET",
-            hostname: "aip.baidubce.com",
-            port: null,
-            path: `/oauth/2.0/token?grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`,
-        };
-
-        var req = https.request(options, function (res) {
-            var chunks = [];
-
-            res.on("data", function (chunk) {
-                chunks.push(chunk);
-            });
-
-            res.on("end", function () {
-                var body = Buffer.concat(chunks);
-                var access_token = JSON.parse(body.toString()).access_token;
-                console.log(access_token);
-                if (!access_token) return callback(body.toString(), null);
-                get_ocr(access_token);
-            });
-
-            res.on("error", () => {
-                return callback(JSON.stringify(err), null);
-            });
-        });
-
-        req.on("error", () => {
-            if (store.get("OCR.离线切换")) {
-                local_ocr(arg, (err, r) => {
-                    return callback(err, r);
-                });
-            } else {
-                return callback("网络或服务错误", null);
-            }
-        });
-
-        req.end();
-    }
-    function get_ocr(access_token) {
-        var url = store.get(`在线OCR.${type}.url`);
-        var hostname = url.replace(/https:\/\/(.*)/, "$1").split("/")[0];
-        var path = url
-            .replace(/https:\/\/(.*)/, "$1")
-            .split("/")
-            .splice(1)
-            .join("/");
-        var options = {
-            method: "POST",
-            hostname: hostname,
-            port: null,
-            path: `/${path}?access_token=${access_token}`,
-            headers: {
-                "content-type": "application/x-www-form-urlencoded",
+        获取(
+            `https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`,
+            {
+                method: "GET",
+                port: null,
             },
-        };
-
-        var req = https.request(options, (res) => {
-            var chunks = [];
-
-            res.on("data", function (chunk) {
-                chunks.push(chunk);
-            });
-
-            res.on("end", function () {
-                var body = Buffer.concat(chunks);
-                console.log(body.toString());
-                format(body.toString());
-            });
-
-            res.on("error", (err) => {
-                return callback(err, null);
-            });
-        });
-
-        req.write(new URLSearchParams({ image: arg, paragraph: "true" }).toString());
-        req.end();
+            (error, result) => {
+                if (error) {
+                    if (store.get("OCR.离线切换")) {
+                        local_ocr(arg, (err, r) => {
+                            return callback(err, r);
+                        });
+                    } else {
+                        return callback("网络或服务错误", null);
+                    }
+                } else {
+                    var access_token = result.access_token;
+                    console.log(access_token);
+                    if (!access_token) return callback(body.toString(), null);
+                    获取(
+                        `${store.get(`在线OCR.${type}.url`)}?access_token=${access_token}`,
+                        {
+                            method: "POST",
+                            port: null,
+                            headers: {
+                                "content-type": "application/x-www-form-urlencoded",
+                            },
+                        },
+                        (error, result) => {
+                            if (error) return callback(error, null);
+                            format(result);
+                        },
+                        { image: arg, paragraph: "true" }
+                    );
+                }
+            }
+        );
     }
-    function format(result) {
-        result = JSON.parse(result);
 
+    function format(result) {
         if (result.error_msg || result.error_code) return callback(JSON.stringify(result), null);
 
         var output = "";
