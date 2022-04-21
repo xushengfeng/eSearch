4c0b3fd6f7f548d94cb3b0475760cbeddaacf4c0
xushengfeng <xushengfeng_zg@163.com>
2022-04-22 00:22:24 +0800

    :zap:历史记录在展开侧栏时保存

--- js/clip_window.js
+++ js/clip_window.js
@@ -63,8 +63,6 @@ ipcRenderer.on("reflash", (a, x, w, h) => {
     d = new ImageData(Uint8ClampedArray.from(x), w, h);
     main_canvas.getContext("2d").putImageData(d, 0, 0);
     final_rect = [0, 0, main_canvas.width, main_canvas.height];
-    if (store.get("记录截屏.记录"))
-        add_img([{ src: main_canvas.toDataURL({ left: 0, top: 0, width: w, height: h, type: "png" }), w, h }]);
 });
 
 function add_img(img_l) {
@@ -75,6 +73,10 @@ function add_img(img_l) {
 }
 
 function show_img_his() {
+    var w = main_canvas.width,
+        h = main_canvas.height;
+    if (store.get("记录截屏.记录"))
+        add_img([{ src: main_canvas.toDataURL({ left: 0, top: 0, width: w, height: h, type: "png" }), w, h }]);
     var img_store = new Store({ name: "img_history" });
     if (document.getElementById("img_history").innerHTML != "") return;
     var o_l = img_store.get("截屏记录") || {};
