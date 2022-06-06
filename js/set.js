c7479003d03b5b5feca035f94a18513f78936b2e
xushengfeng <xushengfeng_zg@163.com>
2022-06-06 21:33:46 +0800

    :speech_balloon:设置 去除多余文字

--- js/set.js
+++ js/set.js
@@ -707,9 +707,7 @@ document.querySelector("#info").innerHTML = `<div>${t("项目主页:")} <a href=
 }</a></div>
     <div><a href="https://github.com/xushengfeng/eSearch/releases/tag/${package.version}">${t("更新日志")}</a></div>
     <div><a href="https://github.com/xushengfeng/eSearch/issues">${t("错误报告与建议")}</a></div>
-    <div>${t("本软件遵循")} <a href="https://www.gnu.org/licenses/gpl-3.0.html">${package.license}</a> ${t(
-    "协议"
-)}</div>
+    <div>${t("本软件遵循")} <a href="https://www.gnu.org/licenses/gpl-3.0.html">${package.license}</a></div>
     <div>${t("本软件基于")} <a href="https://esearch.vercel.app/readme/all_license.json">${t("这些软件")}</a></div>
     <div>Copyright (C) 2021 ${package.author.name} ${package.author.email}</div>`;
 
