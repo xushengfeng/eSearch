1ca8f80a93a77a1d7ef3a208ca62558f2f95d287
xushengfeng <xushengfeng_zg@163.com>
2022-07-11 13:56:36 +0800

    :recycle:代码 关联root.ts

--- js/root.js
+++ js/root.js
@@ -1,6 +1,6 @@
 const Store = require("electron-store");
-store = new Store();
-模糊 = store.get("全局.模糊");
+var store = new Store();
+var 模糊 = store.get("全局.模糊");
 if (模糊 != 0) {
     document.documentElement.style.setProperty("--blur", `blur(${模糊}px)`);
 }
@@ -8,7 +8,7 @@ else {
     document.documentElement.style.setProperty("--blur", `none`);
 }
 document.documentElement.style.setProperty("--alpha", store.get("全局.不透明度"));
-字体 = store.get("字体");
+var 字体 = store.get("字体");
 document.documentElement.style.setProperty("--main-font", 字体.主要字体);
 document.documentElement.style.setProperty("--monospace", 字体.等宽字体);
 const { t, lan } = require("./lib/translate/translate");
