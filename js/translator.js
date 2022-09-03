7dbbba33348a04dee52d55cd25aa050e05e32991
xushengfeng <xushengfeng_zg@163.com>
2022-09-04 00:33:20 +0800

    :sparkles:主要 翻译器

---
+++ js/translator.js
@@ -0,0 +1,21 @@
+const s = decodeURIComponent(location.search);
+const sl = s
+    .slice(1)
+    .split("&")
+    .map((v) => v.split("="));
+for (let i of sl) {
+    switch (i[0]) {
+        case "text":
+            var text = i[1];
+            break;
+        case "from":
+            var fl = i[1];
+            break;
+        case "to":
+            var tl = i[1];
+            break;
+        default:
+            break;
+    }
+}
+document.querySelector("textarea").value = text;
