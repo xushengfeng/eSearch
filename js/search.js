2aede6c345c901c76d5720722bcb40f355fa8227
xushengfeng <xushengfeng_zg@163.com>
2022-09-06 00:16:42 +0800

    :sparkles:搜索 谷歌

--- js/search.js
+++ js/search.js
@@ -47,3 +47,24 @@ fetch(`https://cn.bing.com/search?q=${encodeURIComponent(text)}`)
         document.getElementById("bing").insertAdjacentHTML("beforeend", div);
     }
 });
+fetch(`https://www.google.com/search?q=${encodeURIComponent(text)}`)
+    .then((v) => v.text())
+    .then(async (v) => {
+    let tmp_div = document.createElement("div");
+    tmp_div.innerHTML = v;
+    console.log(tmp_div);
+    let l = [];
+    let sl = tmp_div.querySelectorAll("h3");
+    for await (let i of sl) {
+        let title = i.innerText;
+        let body = i.parentElement.parentElement.parentElement.nextElementSibling.innerText;
+        let href = i.parentElement.href;
+        l.push({ title, body, href });
+    }
+    console.log(l);
+    for (const i of l) {
+        let div = `<div>
+            <h2><a href="${i.href}">${i.title}</a></h2><p>${i.body}</p></div>`;
+        document.getElementById("google").insertAdjacentHTML("beforeend", div);
+    }
+});
