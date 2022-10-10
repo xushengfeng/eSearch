508c510165497267fdbf8081d07d54daab4de82c
xushengfeng <xushengfeng_zg@163.com>
2022-10-10 17:43:20 +0800

    设置 修复路径

--- js/setting_preload.js
+++
@@ -1,8 +0,0 @@
-const Store = require("electron-store");
-var store = new Store();
-const { t, lan } = require("./lib/translate/translate");
-lan(store.get("语言.语言"));
-document.body.innerHTML = document.body.innerHTML
-    .replace(/\{(.*?)\}/g, (m, v) => t(v))
-    .replace(/<t>(.*?)<\/t>/g, (m, v) => t(v));
-document.title = t(document.title);
