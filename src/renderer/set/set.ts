bfba107efdbfa9713f43d13393570e5901c28eea
xushengfeng <xushengfeng_zg@163.com>
2022-10-28 20:08:30 +0800

    设置 可设置是否启用离线ocr node后端

--- src/renderer/set/set.ts
+++ src/renderer/set/set.ts
@@ -549,6 +549,7 @@ document.getElementById("OCR拖拽放置区").ondrop = (e) => {
     set_ocr();
     document.getElementById("OCR拖拽放置区").classList.remove("拖拽突出");
 };
+(<HTMLInputElement>document.getElementById("node后端")).value = store.get("离线OCR配置.node");
 
 (<HTMLInputElement>document.getElementById("baidu_ocr_url")).value = store.get("在线OCR.baidu.url");
 (<HTMLInputElement>document.getElementById("baidu_ocr_id")).value = store.get("在线OCR.baidu.id");
@@ -784,6 +785,7 @@ function save_setting() {
             : false,
         版本: store.get("OCR.版本"),
     });
+    store.set("离线OCR配置.node", (<HTMLInputElement>document.getElementById("node后端")).value);
     store.set("在线OCR.baidu", {
         url: (<HTMLInputElement>document.getElementById("baidu_ocr_url")).value,
         id: (<HTMLInputElement>document.getElementById("baidu_ocr_id")).value,
@@ -954,7 +956,9 @@ function version_new(v1, v2) {
 document.getElementById("info").innerHTML = `<div>${t("项目主页:")} <a href="${package_json.homepage}">${
     package_json.homepage
 }</a></div>
-    <div><a href="https://github.com/xushengfeng/eSearch/releases/tag/${package_json.version}">${t("更新日志")}</a></div>
+    <div><a href="https://github.com/xushengfeng/eSearch/releases/tag/${package_json.version}">${t(
+    "更新日志"
+)}</a></div>
     <div><a href="https://github.com/xushengfeng/eSearch/issues">${t("错误报告与建议")}</a></div>
     <div>${t("本软件遵循")} <a href="https://www.gnu.org/licenses/gpl-3.0.html">${package_json.license}</a></div>
     <div>${t("本软件基于")} <a href="https://esearch.vercel.app/readme/all_license.json">${t("这些软件")}</a></div>
