4b69afb2bf0e255cc36767488e9640b1828514ad
xushengfeng <xushengfeng_zg@163.com>
2022-12-14 22:57:30 +0800

    主要 打包时下载ocr zip文件

--- before_pack.js
+++ before_pack.js
@@ -14,25 +14,13 @@ exports.default = async function () {
 
     if (!fs.existsSync("./ocr/ppocr/默认")) {
         fs.mkdirSync("./ocr/ppocr/默认", { recursive: true });
-        fs.writeFileSync(
-            "./ocr/ppocr/默认/ppocr_det.onnx",
-            await download(
-                "https://github.com/xushengfeng/eSearch-OCR/releases/download/3.0.0/ch_PP-OCRv3_det_infer.onnx",
-                { rejectUnauthorized: false }
-            )
-        );
-        fs.writeFileSync(
-            "./ocr/ppocr/默认/ppocr_rec.onnx",
-            await download(
-                "https://github.com/xushengfeng/eSearch-OCR/releases/download/3.0.0/ch_PP-OCRv3_rec_infer.onnx",
-                { rejectUnauthorized: false }
-            )
-        );
-        fs.writeFileSync(
-            "./ocr/ppocr/默认/ppocr_keys_v1.txt",
-            await download("https://github.com/xushengfeng/eSearch-OCR/releases/download/3.0.0/ppocr_keys_v1.txt", {
+        await download(
+            "https://github.com/xushengfeng/eSearch-OCR/releases/download/3.0.0/ch.zip",
+            "./ocr/ppocr/默认/",
+            {
+                extract: true,
                 rejectUnauthorized: false,
-            })
+            }
         );
     }
     if (process.platform == "win32" && !fs.existsSync("./build/vc_redist.x64.exe")) {
