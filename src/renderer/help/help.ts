2effd755caa21779780a2826ab1dfe392f4b0b43
xsf_win11 <xushengfeng_zg@163.com>
2023-01-05 20:27:28 +0800

    帮助 语言识别修复

--- src/renderer/help/help.ts
+++ src/renderer/help/help.ts
@@ -10,7 +10,7 @@ const Store = require("electron-store");
 var store = new Store({
     cwd: config_path || "",
 });
-if (store.get("语言.语言") != "zh-CN") {
+if (store.get("语言.语言") != "zh-HANS") {
     let href = `http://fanyi.baidu.com/transpage?query=https%3A%2F%2Fe-search.vercel.app%2Fhelp.html&from=zh&to=${
         store.get("语言.语言").split("-")[0]
     }&source=url&render=1`;
