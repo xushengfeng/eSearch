bf71b2d9af726c83fd03cc6e00cece56f9df4b73
xushengfeng <xushengfeng_zg@163.com>
2023-01-17 20:21:04 +0800

    主页面 加入图片展示区

--- src/renderer/js/js.ts
+++ src/renderer/js/js.ts
@@ -1732,6 +1732,7 @@ function google(image, callback) {
 /************************************OCR */
 
 function ocr(img: string, type: string | "baidu" | "youdao", callback: Function) {
+    add_ocr_photo(img);
     if (type == "baidu" || type == "youdao") {
         online_ocr(type, img, (err, r) => {
             return callback(err, r);
@@ -1903,3 +1904,137 @@ function online_ocr(type: string, arg: string, callback: Function) {
     }
 }
 // online_ocr();
+
+const drop_el = document.getElementById("drop");
+const imgs_el = document.getElementById("img_view");
+const upload_pel = document.getElementById("file_input");
+const upload_el = document.getElementById("upload") as HTMLInputElement;
+const output_el = document.getElementById("r") as HTMLTextAreaElement;
+const run_el = document.getElementById("run");
+
+drop_el.ondragover = (e) => {
+    e.preventDefault();
+};
+drop_el.ondrop = (e) => {
+    e.preventDefault();
+    put_datatransfer(e.dataTransfer);
+};
+drop_el.onpaste = (e) => {
+    e.preventDefault();
+    put_datatransfer(e.clipboardData);
+};
+upload_pel.onclick = () => {
+    upload_el.click();
+};
+upload_el.onchange = () => {
+    let files = upload_el.files;
+    for (let f of files) {
+        let type = f.type.split("/")[0];
+        if (type != "image") continue;
+        let reader = new FileReader();
+        reader.readAsDataURL(f);
+        reader.onload = () => {
+            let el = create_img(reader.result as string);
+            imgs_el.append(el);
+        };
+    }
+};
+run_el.classList.add("no_run");
+run_el.onclick = () => {
+    run_ocr();
+};
+document.getElementById("close").onclick = () => {
+    output_el.value = "";
+    output = [];
+    imgs_el.innerHTML = "";
+};
+
+var ocr_init = false;
+
+/** 拖放数据处理 */
+function put_datatransfer(data: DataTransfer) {
+    if (data.files.length != 0) {
+        for (let f of data.files) {
+            let type = f.type.split("/")[0];
+            if (type != "image") continue;
+            let reader = new FileReader();
+            reader.readAsDataURL(f);
+            reader.onload = () => {
+                let el = create_img(reader.result as string);
+                imgs_el.append(el);
+            };
+        }
+    } else {
+    }
+}
+
+function create_img(src: string) {
+    let div = document.createElement("div");
+    div.classList.add("img_el");
+    let image = document.createElement("img");
+    image.src = src;
+    image.onload = () => {
+        image.setAttribute("data-w", String(image.width));
+        image.setAttribute("data-h", String(image.height));
+    };
+    div.append(image);
+    return div;
+}
+
+function run_ocr() {
+    if (!ocr_init) return;
+    output_el.value = "";
+    output = [];
+    imgs_el.querySelectorAll(":scope > div > div").forEach((el: HTMLElement) => {
+        el.innerHTML = "";
+    });
+    let type = "";
+    imgs_el.querySelectorAll(":scope > div > img").forEach((el: HTMLImageElement, i) => {
+        if (type == "baidu" || type == "youdao") {
+            online_ocr(type, el.src, (err, r) => {
+                add_ocr_text(r, i);
+            });
+        } else {
+            local_ocr(type, el.src, (err, r) => {
+                add_ocr_text(r, i);
+            });
+        }
+    });
+}
+
+function add_ocr_text(
+    r: { text: string; box: { x: number; y: number; w: number; h: number; a: number } }[],
+    i: number
+) {
+    let img = imgs_el.querySelectorAll("img")[i];
+    let canvas = document.createElement("canvas");
+    let w = img.naturalWidth,
+        h = img.naturalHeight;
+    canvas.width = w;
+    canvas.height = h;
+    canvas.getContext("2d").drawImage(img, 0, 0);
+
+    let div = document.createElement("div");
+    for (let i of r) {
+        if (!i.text) continue;
+        let x0 = i.box[0][0];
+        let y0 = i.box[0][1];
+        let x1 = i.box[2][0];
+        let y1 = i.box[2][1];
+        let xel = document.createElement("p");
+        xel.style.left = `${(x0 / w) * 100}%`;
+        xel.style.top = `${(y0 / h) * 100}%`;
+        xel.style.width = `${((x1 - x0) / w) * 100}%`;
+        xel.style.height = `${((y1 - y0) / h) * 100}%`;
+        div.append(xel);
+        xel.innerText = i.text;
+    }
+    img.parentElement.append(div);
+}
+
+function add_ocr_photo(base: string) {
+    let el = create_img("data:image/png;base64," + base);
+    imgs_el.append(el);
+}
+
+let output = [];
