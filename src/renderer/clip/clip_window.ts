743bbf628b40b3925111a79182f63c968095db20
xushengfeng <xushengfeng_zg@163.com>
2023-08-12 21:42:50 +0800

    截屏 尝试修复win多屏幕识别

--- src/renderer/clip/clip_window.ts
+++ src/renderer/clip/clip_window.ts
@@ -171,7 +171,10 @@ function capturer(all: Screenshots[]) {
         for (let s of displays) {
             for (let ss of x) {
                 // id对不上，只能用坐标，考虑精度，小于10认为是相等的
-                if (ss.x - s.bounds.x * s.scaleFactor < 10 && ss.y - s.bounds.y * s.scaleFactor < 10) {
+                if (
+                    ss.x * ss.scaleFactor - s.bounds.x * s.scaleFactor < 10 &&
+                    ss.y * ss.scaleFactor - s.bounds.y * s.scaleFactor < 10
+                ) {
                     ss.x = s.bounds.x;
                     ss.y = s.bounds.y;
                     ss.height = s.size.height;
