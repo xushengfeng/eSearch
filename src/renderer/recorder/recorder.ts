79a2c6284195a0178170978f5d6e9eec640b2245
xushengfeng <xushengfeng_zg@163.com>
2024-03-02 14:41:55 +0800

    录屏 报错日志显示命令

--- src/renderer/recorder/recorder.ts
+++ src/renderer/recorder/recorder.ts
@@ -757,7 +757,11 @@ function updataPrEl(pr: typeof ffprocess) {
                 document.getElementById("log_p").classList.remove("hide_log");
                 for (let i in pr[key]) {
                     if (pr[key][i].finish === "err") {
-                        logText.value += "\n" + pr[key][i].logs.map((i) => i.text).join("\n");
+                        logText.value +=
+                            "\n命令：\n" +
+                            pr[key][i].testCom +
+                            "\n\n输出：\n" +
+                            pr[key][i].logs.map((i) => i.text).join("\n");
                     }
                 }
                 logText.scrollTop = logText.scrollHeight;
