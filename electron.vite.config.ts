8e81af0f70752ff7bded745d913c4889fb1e2c85
xushengfeng <xushengfeng_zg@163.com>
2024-03-22 16:43:49 +0800

    主要 修复图标错误

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
                     translator: path.resolve(__dirname, "src/renderer/translator.html"),
                 },
             },
+            assetsInlineLimit: 0,
         },
     },
 });
