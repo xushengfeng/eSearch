841232cfce0cabe2224f063c7338773c6ce11ef7
xushengfeng <xushengfeng_zg@163.com>
2024-03-29 12:49:36 +0800

    翻译 新语言

--- lib/translate/readme.md
+++ lib/translate/readme.md
@@ -1,5 +1,8 @@
 # 翻译 Translate
 
+创建 csv，指定存在的语言进行编辑或指定新语言
+Create a csv, specify an existing language for editing or specify a new language
+
 ```shell
 node -l en
 # en.csv
--- src/renderer/set/set.ts
+++ src/renderer/set/set.ts
@@ -1079,6 +1079,7 @@ document.getElementById("info").innerHTML = `<div>${t("项目主页:")} <a href=
 }</a></div>
     <div><a href="https://github.com/xushengfeng/eSearch/releases/tag/${packageJson.version}">${t("更新日志")}</a></div>
     <div><a href="https://github.com/xushengfeng/eSearch/issues">${t("错误报告与建议")}</a></div>
+    <div><a href="https://github.com/xushengfeng/eSearch/tree/master/lib/translate">${t("改进翻译")}</a></div>
     <div>${t("本软件遵循")} <a href="https://www.gnu.org/licenses/gpl-3.0.html">${packageJson.license}</a></div>
     <div>${t("本软件基于")} <a href="https://esearch.vercel.app/readme/all_license.json">${t("这些软件")}</a></div>
     <div>Copyright (C) 2021 ${packageJson.author.name} ${packageJson.author.email}</div>`;
