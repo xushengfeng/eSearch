1e1548b4a9a3656e3f920f0a1a78fa6ce4468be3
xushengfeng <xushengfeng_zg@163.com>
2024-07-17 12:39:54 +0800

    翻译 使用新框架

--- src/renderer/translate/translate.ts
+++ src/renderer/translate/translate.ts
@@ -9,86 +9,94 @@ var store = new Store({
     cwd: configPath || "",
 });
 
+import { button, ele, frame, image, p, pureStyle, txt, view } from "dkh-ui";
+
+pureStyle();
+
 initStyle(store);
 
 import copy_svg from "../assets/icons/copy.svg";
 
-function iconEl(img: string) {
-    return el("img", { src: img, class: "icon" });
+function iconButton(img: string) {
+    return button(image(img, "icon").class("icon").style({ width: "100%", height: "100%" })).style({
+        position: "relative",
+    });
 }
 
-import { el } from "redom";
+const input = ele("textarea").style({ width: "100%", padding: "8px", resize: "vertical" });
+const lans = view("x");
+const lansFrom = ele("select").on("change", () => {
+    translate(input.el.value);
+});
+const lansTo = ele("select").on("change", () => {
+    translate(input.el.value);
+});
 
-const input = el("textarea");
-const lans = el("div");
-const lansFrom = el("select");
-const lansTo = el("select");
+const results = view("y").style({ gap: "8px", margin: "8px" });
 
-const results = el("div", { class: "results" });
+lans.add([lansFrom, lansTo]);
 
-lansFrom.onchange = () => {
-    translate(input.value);
-};
-lansTo.onchange = () => {
-    translate(input.value);
-};
-lans.append(lansFrom, lansTo);
-
-document.body.append(input, lans, results);
+document.body.append(input.el, lans.el, results.el);
 
 const inputText = decodeURIComponent(new URLSearchParams(location.search).get("text"));
 
 const fyq = store.get("翻译.翻译器") as setting["翻译"]["翻译器"];
 
 function translate(text: string) {
-    results.innerHTML = "";
+    results.el.innerHTML = "";
     if (!text.trim()) return;
     for (let i of fyq) {
-        const copy = el("button", iconEl(copy_svg));
-        const e = el("div", el("div", { class: "title" }, el("span", i.name), copy));
-        results.append(e);
-        const t = el("p");
-        e.append(t);
+        const copy = iconButton(copy_svg).style({ width: "24px", height: "24px" });
+        const e = frame(`result${i.id}`, {
+            0: view().style({ width: "100%" }),
+            _: { title: view("x").style({ "align-items": "center" }), name: txt(i.name), copy },
+            content: p(""),
+        });
+        results.add(e.el);
+        const c = e.els.content;
         translateI(text, i).then((text) => {
-            t.innerText = text;
-            copy.onclick = () => {
+            c.el.innerText = text;
+            copy.on("click", () => {
                 navigator.clipboard.writeText(text);
-            };
+            });
         });
     }
 }
 
 function translateI(text: string, i: setting["翻译"]["翻译器"][0]) {
     xtranslator.e[i.type].setKeys(i.keys);
-    return xtranslator.e[i.type].run(text, lansFrom.value, lansTo.value);
+    return xtranslator.e[i.type].run(text, lansFrom.el.value, lansTo.el.value);
 }
 
 const e = xtranslator.e[fyq[0].type];
 if (e) {
     const mainLan = store.get("语言.语言");
-    e.getLanT({ text: mainLan, sort: "text" }).forEach((v) => {
-        lansFrom.append(el("option", v.text, { value: v.lan }));
-    });
-    e.getTargetLanT({ text: mainLan, sort: "text" }).forEach((v) => {
-        lansTo.append(el("option", v.text, { value: v.lan }));
-    });
+    lansFrom.add(
+        e.getLanT({ text: mainLan, sort: "text" }).map((v) => ele("option").add(txt(v.text)).attr({ value: v.lan })),
+        10
+    );
+    lansTo.add(
+        e
+            .getTargetLanT({ text: mainLan, sort: "text" })
+            .map((v) => ele("option").add(txt(v.text)).attr({ value: v.lan })),
+        10
+    );
 }
 
-input.value = inputText;
+input.el.value = inputText;
 if (inputText) {
     translate(inputText);
 }
 
 let composing = false;
-input.addEventListener("compositionstart", () => (composing = true));
-input.addEventListener("compositionend", () => (composing = false));
+input.on("compositionstart", () => (composing = true)).on("compositionend", () => (composing = false));
 
 let lastTrans: NodeJS.Timeout;
 
-input.oninput = () => {
+input.on("input", () => {
     if (composing) return;
     if (lastTrans) clearTimeout(lastTrans);
     lastTrans = setTimeout(() => {
-        translate(input.value);
+        translate(input.el.value);
     }, 2000);
-};
+});
