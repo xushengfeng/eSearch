147d42ab8fa4be20b05f05d185651e249f94e974
xushengfeng <xushengfeng_zg@163.com>
2024-09-05 15:52:37 +0800

    贴图 使用dkh-ui

--- src/renderer/ding/ding.ts
+++ src/renderer/ding/ding.ts
@@ -5,6 +5,7 @@ const path = require("node:path") as typeof import("path");
 const os = require("node:os") as typeof import("os");
 import initStyle from "../root/root";
 import store from "../../../lib/store/renderStore";
+import { elFromId, type ElType, image, pack, view } from "dkh-ui";
 initStyle(store);
 
 ipcRenderer.on("ding", (_event, type, id, more) => {
@@ -20,16 +21,11 @@ ipcRenderer.on("ding", (_event, type, id, more) => {
             mouseEnd();
             break;
         case "back":
-            back2(document.getElementById(id));
+            back2(elFromId(id));
             break;
         case "resize":
             if (!resizeSender)
-                resize(
-                    document.getElementById(more.id),
-                    more.zoom,
-                    more.dx,
-                    more.dy,
-                );
+                resize(elFromId(more.id), more.zoom, more.dx, more.dy);
             break;
     }
 });
@@ -42,7 +38,7 @@ function sendEvent(
     ipcRenderer.send("ding_event", type, id, more);
 }
 
-const dives: HTMLElement[] = [];
+const dives: ElType<HTMLElement>[] = [];
 
 let changing: { x: number; y: number } = null;
 const photos: { [key: string]: [number, number, number, number] } = {};
@@ -50,35 +46,32 @@ const urls = {};
 ipcRenderer.on("img", (_event, wid, x, y, w, h, url) => {
     photos[wid] = [x, y, w, h];
     urls[wid] = url;
-    const div = document.createElement("div");
+    const div = view().attr({ id: wid }).class("ding_photo");
     dives.push(div);
-    div.id = wid;
-    div.className = "ding_photo";
-    if (store.get("贴图.窗口.提示")) div.classList.add("ding_photo_h");
-    div.style.left = `${x}px`;
-    div.style.top = `${y}px`;
-    div.style.width = `${w}px`;
-    div.style.height = `${h}px`;
-    const img = document.createElement("img");
-    img.draggable = false;
-    img.src = url;
-    img.className = "img";
+    if (store.get("贴图.窗口.提示")) div.class("ding_photo_h");
+    div.style({
+        left: `${x}px`,
+        top: `${y}px`,
+        width: `${w}px`,
+        height: `${h}px`,
+    });
+    const img = image(url, "").attr({ draggable: false }).class("img");
     const toolBar = document
         .querySelector("#tool_bar")
         .cloneNode(true) as HTMLElement;
     (<HTMLElement>toolBar.querySelector("#tool_bar_c")).style.display = "flex";
     // 顶栏
-    div.onmouseenter = () => {
+    div.el.onmouseenter = () => {
         (<HTMLElement>toolBar.querySelector("#tool_bar_c")).style.transform =
             "translateY(0)";
     };
-    div.onmouseleave = () => {
+    div.el.onmouseleave = () => {
         (<HTMLElement>toolBar.querySelector("#tool_bar_c")).style.transform =
             "translateY(-105%)";
     };
     // 透明
     (<HTMLElement>toolBar.querySelector("#透明度")).oninput = () => {
-        img.style.opacity = `${Number((<HTMLInputElement>toolBar.querySelector("#透明度")).value) / 100}`;
+        img.el.style.opacity = `${Number((<HTMLInputElement>toolBar.querySelector("#透明度")).value) / 100}`;
         (<HTMLElement>(
             toolBar.querySelector("#透明度_p")
         )).innerHTML = `${(<HTMLInputElement>toolBar.querySelector("#透明度")).value}%`;
@@ -128,10 +121,10 @@ ipcRenderer.on("img", (_event, wid, x, y, w, h, url) => {
         }
     };
     // 滚轮缩放
-    div.onwheel = (e) => {
+    div.el.onwheel = (e) => {
         if (e.deltaY !== 0) {
             let zoom =
-                Number(div.querySelector("#size > span").innerHTML) / 100;
+                Number(div.el.querySelector("#size > span").innerHTML) / 100;
             const zz = 1 + Math.abs(e.deltaY) / 300;
             zoom = e.deltaY > 0 ? zoom / zz : zoom * zz;
             if (zoom < 0.05) zoom = 0.05;
@@ -139,45 +132,44 @@ ipcRenderer.on("img", (_event, wid, x, y, w, h, url) => {
             resize(
                 div,
                 zoom,
-                e.offsetX / div.offsetWidth,
-                e.offsetY / div.offsetHeight,
+                e.offsetX / div.el.offsetWidth,
+                e.offsetY / div.el.offsetHeight,
             );
             resizeSender = false;
         }
     };
-    // 三个按钮
     (<HTMLElement>toolBar.querySelector("#minimize")).onclick = () => {
-        minimize(div);
+        minimize(div.el);
     };
     (<HTMLElement>toolBar.querySelector("#back")).onclick = () => {
-        back(div);
+        back(wid);
     };
     (<HTMLElement>toolBar.querySelector("#close")).onclick = () => {
-        close(div);
+        close(wid);
     };
     (<HTMLElement>toolBar.querySelector("#copy")).onclick = () => {
-        copy(div);
+        copy(wid);
     };
     (<HTMLElement>toolBar.querySelector("#save")).onclick = () => {
-        save(div);
+        save(wid);
     };
     (<HTMLElement>toolBar.querySelector("#edit")).onclick = () => {
-        edit(div);
+        edit(wid);
     };
     // 双击行为
-    div.ondblclick = () => {
-        if (store.get("贴图.窗口.双击") === "归位") back(div);
-        else close(div);
+    div.el.ondblclick = () => {
+        if (store.get("贴图.窗口.双击") === "归位") back(wid);
+        else close(wid);
     };
     // 放到前面
-    div.onclick = () => {
-        div.style.zIndex = String(toppest + 1);
+    div.el.onclick = () => {
+        div.el.style.zIndex = String(toppest + 1);
         document.getElementById("dock").style.zIndex = String(toppest + 2);
         toppest += 1;
     };
-    div.appendChild(toolBar);
-    div.appendChild(img);
-    document.querySelector("#photo").appendChild(div);
+    div.add(toolBar);
+    div.add(img);
+    document.querySelector("#photo").appendChild(div.el);
 
     // dock
     dockI();
@@ -233,33 +225,35 @@ function transform(el, v) {
         el.querySelector(".img").classList.remove("tran");
     }
 }
-function back(el: HTMLElement) {
-    sendEvent("back", el.id);
+function back(id: string) {
+    sendEvent("back", id);
 }
-function back2(el: HTMLElement) {
-    el.style.transition = "var(--transition)";
+function back2(el: ElType<HTMLElement>) {
+    el.el.style.transition = "var(--transition)";
     setTimeout(() => {
-        el.style.transition = "";
+        el.el.style.transition = "";
         resizeSender = true;
         resize(el, 1, 0, 0);
         resizeSender = false;
     }, 400);
-    const pS = photos[el.id];
-    el.style.left = `${pS[0]}px`;
-    el.style.top = `${pS[1]}px`;
-    el.style.width = `${pS[2]}px`;
-    el.style.height = `${pS[3]}px`;
-    ipcRenderer.send("ding_p_s", el.id, pS);
+    const pS = photos[el.el.id];
+    el.style({
+        left: `${pS[0]}px`,
+        top: `${pS[1]}px`,
+        width: `${pS[2]}px`,
+        height: `${pS[3]}px`,
+    });
+    ipcRenderer.send("ding_p_s", el.el.id, pS);
 
-    (el.querySelector("#透明度") as HTMLInputElement).value = "100";
-    el.querySelector("#透明度_p").innerHTML = "100%";
-    (el.querySelector(".img") as HTMLImageElement).style.opacity = "1";
+    (el.el.querySelector("#透明度") as HTMLInputElement).value = "100";
+    el.el.querySelector("#透明度_p").innerHTML = "100%";
+    (el.el.querySelector(".img") as HTMLImageElement).style.opacity = "1";
 }
-function close(el: HTMLElement) {
+function close(id: string) {
     ipcRenderer.send(
         "ding_event",
         "close",
-        el.id,
+        id,
         Object.keys(photos).length === 1,
     );
 }
@@ -269,21 +263,21 @@ function close2(el: HTMLElement) {
     delete urls[el.id];
     dockI();
 }
-function copy(el: HTMLElement) {
-    clipboard.writeImage(nativeImage.createFromDataURL(urls[el.id]));
+function copy(id: string) {
+    clipboard.writeImage(nativeImage.createFromDataURL(urls[id]));
 }
-function save(el: HTMLElement) {
+function save(id: string) {
     const b = Buffer.from(
-        urls[el.id].replace(/^data:image\/\w+;base64,/, ""),
+        urls[id].replace(/^data:image\/\w+;base64,/, ""),
         "base64",
     );
     // todo 自动保存
     const save = ipcRenderer.sendSync("get_save_file_path", "png");
     fs.writeFileSync(save, b);
 }
-function edit(el: HTMLElement) {
+function edit(id: string) {
     const b = Buffer.from(
-        urls[el.id].replace(/^data:image\/\w+;base64,/, ""),
+        urls[id].replace(/^data:image\/\w+;base64,/, ""),
         "base64",
     );
     const save = path.join(
@@ -321,15 +315,15 @@ type Resize = {
 
 document.onmousedown = (e) => {
     const el = e.target as HTMLElement;
-    const div = dives.find((d) => d.contains(el));
+    const div = dives.find((d) => d.el.contains(el));
     if (div && (el.id === "tool_bar_c" || el.tagName === "IMG")) {
         sendEvent("move_start", null, {
-            id: div.id,
+            id: div.el.id,
             x: e.clientX,
             y: e.clientY,
-            dx: e.offsetX / div.offsetWidth,
-            dy: e.offsetY / div.offsetHeight,
-            d: dire(div, { x: e.clientX, y: e.clientY }),
+            dx: e.offsetX / div.el.offsetWidth,
+            dy: e.offsetY / div.el.offsetHeight,
+            d: dire(div.el, { x: e.clientX, y: e.clientY }),
         } as start);
     }
     resizeSender = true;
@@ -346,11 +340,11 @@ function mouseStart(op: start) {
 }
 function mouseMove(el: HTMLElement, x: number, y: number) {
     if (direction) {
-        move(windowDiv, { x, y });
+        move(pack(windowDiv), { x, y });
     } else {
-        const div = dives.find((d) => d.contains(el));
+        const div = dives.find((d) => d.el.contains(el));
         if (div && (el.id === "tool_bar_c" || el.tagName === "IMG")) {
-            const d = dire(div, { x, y });
+            const d = dire(div.el, { x, y });
             cursor(d);
         }
     }
@@ -428,7 +422,7 @@ function cursor(d: string) {
     document.querySelector("html").style.cursor = m[d];
 }
 
-function move(el: HTMLElement, e: { x: number; y: number }) {
+function move(el: ElType<HTMLElement>, e: { x: number; y: number }) {
     if (changing != null && oPs.length !== 0) {
         const oE = changing;
         const dx = e.x - oE.x;
@@ -505,67 +499,69 @@ function move(el: HTMLElement, e: { x: number; y: number }) {
             }
             case "move":
                 pS = [ox + dx, oy + dy, ow, oh];
-                el.style.left = `${pS[0]}px`;
-                el.style.top = `${pS[1]}px`;
-                el.style.width = `${pS[2]}px`;
-                el.style.height = `${pS[3]}px`;
+                el.style({
+                    left: `${pS[0]}px`,
+                    top: `${pS[1]}px`,
+                    width: `${pS[2]}px`,
+                    height: `${pS[3]}px`,
+                });
                 return;
         }
-        resize(el, pS[2] / photos[el.id][2], zp.x, zp.y);
+        resize(el, pS[2] / photos[el.el.id][2], zp.x, zp.y);
     }
 }
 
-function resize(el: HTMLElement, zoom: number, dx: number, dy: number) {
-    (el.querySelector("#size > span") as HTMLElement).innerHTML = String(
-        Math.round(zoom * 100),
-    );
-    const rect = [el.offsetLeft, el.offsetTop, el.offsetWidth, el.offsetHeight];
-    const toWidth = photos[el.id][2] * zoom;
-    const toHeight = photos[el.id][3] * zoom;
+function resize(el: ElType<HTMLElement>, zoom: number, dx: number, dy: number) {
+    const id = el.el.id;
+    el.query("#size > span").el.innerHTML = String(Math.round(zoom * 100));
+    const rect = [
+        el.el.offsetLeft,
+        el.el.offsetTop,
+        el.el.offsetWidth,
+        el.el.offsetHeight,
+    ];
+    const toWidth = photos[id][2] * zoom;
+    const toHeight = photos[id][3] * zoom;
     const point = { x: rect[0] + rect[2] * dx, y: rect[1] + rect[3] * dy };
     const x = point.x - toWidth * dx;
     const y = point.y - toHeight * dy;
     const pS = [x, y, toWidth, toHeight];
-    const bar = el.querySelector("#tool_bar_c") as HTMLElement;
+    const bar = el.query("#tool_bar_c");
     const w = pS[2];
     if (w <= 240) {
-        bar.style.flexDirection = "column";
+        bar.el.style.flexDirection = "column";
     } else {
-        bar.style.flexDirection = "";
+        bar.el.style.flexDirection = "";
     }
+    let zoomN = "";
     if (w <= 100) {
-        // @ts-ignore
-        bar.style.zoom = "0.3";
+        zoomN = "0.3";
     } else if (w <= 130) {
-        // @ts-ignore
-        bar.style.zoom = "0.4";
+        zoomN = "0.4";
     } else if (w <= 300) {
-        // @ts-ignore
-        bar.style.zoom = "0.5";
+        zoomN = "0.5";
     } else if (w <= 340) {
-        // @ts-ignore
-        bar.style.zoom = "0.6";
+        zoomN = "0.6";
     } else if (w <= 380) {
-        // @ts-ignore
-        bar.style.zoom = "0.7";
+        zoomN = "0.7";
     } else if (w <= 420) {
-        // @ts-ignore
-        bar.style.zoom = "0.8";
+        zoomN = "0.8";
     } else if (w <= 500) {
-        // @ts-ignore
-        bar.style.zoom = "0.9";
+        zoomN = "0.9";
     } else {
-        // @ts-ignore
-        bar.style.zoom = "";
+        zoomN = "";
     }
+    bar.style({ zoom: zoomN });
 
-    el.style.left = `${pS[0]}px`;
-    el.style.top = `${pS[1]}px`;
-    el.style.width = `${pS[2]}px`;
-    el.style.height = `${pS[3]}px`;
+    el.style({
+        left: `${pS[0]}px`,
+        top: `${pS[1]}px`,
+        width: `${pS[2]}px`,
+        height: `${pS[3]}px`,
+    });
 
     if (resizeSender)
-        sendEvent("resize", null, { id: el.id, zoom, dx, dy } as Resize);
+        sendEvent("resize", null, { id: id, zoom, dx, dy } as Resize);
 }
 
 const dockP = store.get("ding_dock");
@@ -659,7 +655,7 @@ function dockI() {
                         }, 400);
                         div.classList.remove("minimize");
                     } else {
-                        back(div);
+                        back(div.id);
                     }
                     div.style.zIndex = String(toppest + 1);
                     toppest += 1;
@@ -668,7 +664,7 @@ function dockI() {
             const iClose = dockItem.querySelector("#i_close") as HTMLElement;
             iClose.style.display = "block";
             iClose.onclick = () => {
-                close(document.getElementById(i));
+                close(i);
             };
             const iIgnore = dockItem.querySelector("#i_ignore") as HTMLElement;
             iIgnore.style.display = "block";
