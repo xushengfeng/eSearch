24d448804600e550e57ff6846ebfb8b9fbd721da
xushengfeng <xushengfeng_zg@163.com>
2024-09-06 23:33:32 +0800

    代码 初始化样式抽象

--- src/renderer/root/root.ts
+++ src/renderer/root/root.ts
@@ -6,15 +6,12 @@ function initStyle(
     }
     const 模糊 = store.get("全局.模糊");
     if (模糊 !== 0) {
-        document.documentElement.style.setProperty("--blur", `blur(${模糊}px)`);
+        setCSSVar("--blur", `blur(${模糊}px)`);
     } else {
-        document.documentElement.style.setProperty("--blur", "none");
+        setCSSVar("--blur", "none");
     }
 
-    document.documentElement.style.setProperty(
-        "--alpha",
-        `${store.get("全局.不透明度") * 100}%`,
-    );
+    setCSSVar("--alpha", `${store.get("全局.不透明度") * 100}%`);
 
     const theme = store.get("全局.主题");
     setCSSVar("--bar-bg0", theme.light.barbg);
@@ -29,8 +26,8 @@ function initStyle(
     setCSSVar("--d-icon-color", theme.dark.iconColor);
 
     const 字体 = store.get("字体");
-    document.documentElement.style.setProperty("--main-font", 字体.主要字体);
-    document.documentElement.style.setProperty("--monospace", 字体.等宽字体);
+    setCSSVar("--main-font", 字体.主要字体);
+    setCSSVar("--monospace", 字体.等宽字体);
 }
 
 export default initStyle;
