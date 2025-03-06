741579f6c8f2364e74d3f05241d2e3be0897b0de
xushengfeng <xushengfeng_zg@163.com>
2025-03-06 23:12:44 +0800

    开发 移动镜像生成代码

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -360,37 +360,3 @@ for (let i of ignoreDir) {
 }
 
 module.exports = build;
-
-/**
- * @type {Record<string,Record<string,string[]>>}
- */
-const release = {
-    win32: { gh: ["exe", "zip"], arch: ["x64", "arm64"] },
-    linux: { gh: ["AppImage", "deb", "rpm", "tar.gz"], arch: ["x64", "arm64"] },
-    darwin: { gh: ["dmg", "zip"], arch: ["x64", "arm64"] },
-};
-
-/**
- *
- * @param {string} url
- */
-function getUrl(url) {
-    const version = require("./package.json").version;
-    let t = "| | Windows | macOS | Linux|\n| --- | --- | --- | --- |\n";
-    for (const arch of ["x64", "arm64"]) {
-        t += `|${arch}| `;
-        for (const p of ["win32", "darwin", "linux"]) {
-            if (!release[p].arch.includes(arch)) continue;
-            t += `${(release[p].gh || []).map((i) => `[${i}](${url.replaceAll("$v", version).replace("$arch", arch).replace("$p", p).replace("$h", i)})`).join(" ")}|`;
-        }
-        t += "\n";
-    }
-    return t;
-}
-
-console.log(
-    "⚡镜像下载：\n",
-    getUrl(
-        "https://gh.llkk.cc/https://github.com/xushengfeng/eSearch/releases/download/$v/eSearch-$v-$p-$arch.$h",
-    ),
-);
---
+++ script/release_fast_download.ts
@@ -0,0 +1,26 @@
+const release: Record<string, Record<string, string[]>> = {
+    win32: { gh: ["exe", "zip"], arch: ["x64", "arm64"] },
+    linux: { gh: ["AppImage", "deb", "rpm", "tar.gz"], arch: ["x64", "arm64"] },
+    darwin: { gh: ["dmg", "zip"], arch: ["x64", "arm64"] },
+};
+
+function getUrl(url: string) {
+    const version = require("../package.json").version;
+    let t = "| | Windows | macOS | Linux|\n| --- | --- | --- | --- |\n";
+    for (const arch of ["x64", "arm64"]) {
+        t += `|${arch}| `;
+        for (const p of ["win32", "darwin", "linux"]) {
+            if (!release[p].arch.includes(arch)) continue;
+            t += `${(release[p].gh || []).map((i) => `[${i}](${url.replaceAll("$v", version).replace("$arch", arch).replace("$p", p).replace("$h", i)})`).join(" ")}|`;
+        }
+        t += "\n";
+    }
+    return t;
+}
+
+console.log(
+    "⚡镜像下载：\n",
+    getUrl(
+        "https://gh.llkk.cc/https://github.com/xushengfeng/eSearch/releases/download/$v/eSearch-$v-$p-$arch.$h",
+    ),
+);
