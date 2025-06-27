ce8ea355a811d80c889022763080077f934b7722
xushengfeng <xushengfeng_zg@163.com>
2025-06-27 19:06:22 +0800

    主要 更新typedEntries

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,8 +2,8 @@ export { typedEntries, typedKeys };
 
 // biome-ignore lint/suspicious/noExplicitAny: 相信ai
 type Entry<T> = T extends any ? { [K in keyof T]: [K, T[K]] }[keyof T] : never;
-function typedEntries<T extends object>(obj: T): Entry<T>[] {
-    return Object.entries(obj) as Entry<T>[];
+function typedEntries<T extends object>(obj: T) {
+    return Object.entries(obj) as Exclude<Entry<T>, undefined>[];
 }
 
 function typedKeys<T extends object>(obj: T): (keyof T)[] {
