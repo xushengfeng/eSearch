a0fb8264bfeef66a096e68321374d0dcb7f46674
xushengfeng <xushengfeng_zg@163.com>
2025-08-30 22:23:13 +0800

    代码 移动`utils`

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-export { typedEntries, typedKeys };
+export { typedEntries, typedKeys, tryx, tryD };
 
 // biome-ignore lint/suspicious/noExplicitAny: 相信ai
 type Entry<T> = T extends any ? { [K in keyof T]: [K, T[K]] }[keyof T] : never;
@@ -8,4 +8,20 @@ function typedEntries<T extends object>(obj: T) {
 
 function typedKeys<T extends object>(obj: T): (keyof T)[] {
     return Object.keys(obj) as (keyof T)[];
+}
+
+function tryx<T>(fn: () => T): [T, null] | [null, Error] {
+    try {
+        return [fn(), null];
+    } catch (e) {
+        return [null, e as Error];
+    }
+}
+
+function tryD<T>(fn: () => T, defaultV: T): T {
+    try {
+        return fn();
+    } catch {
+        return defaultV;
+    }
 }
